587e4843de9ae287de40ac797d187bad3f46fcdf
Jorge M. Carvalho <JorgeMiguelSCarvalho@gmail.com>
2017-05-13 16:08:05 +0100

    Added Utility Scripts.

---
+++ GFF-Unity/Assets/Scripts/Helpers/AnimationDestroy.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Used for destroying or disabling objects at the end of animations.
+public class AnimationDestroy : MonoBehaviour {
+
+	// Called by the Animation, destroys this object.
+	public void DestroyAnimation()
+    {
+        Destroy(gameObject);
+    }
+
+    // Called by the animation, disables this object.
+    public void DisableAnimation()
+    {
+        gameObject.SetActive(false);
+    }
+}
